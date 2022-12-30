a3409bec5fea7a55da0aee4801bb9b9ec535d8ef
KASHIF KHAN <102716348+TEAM-KRS@users.noreply.github.com>
2022-12-30 17:19:00 +0500

    Update KRS.py

--- KRS.py
+++ KRS.py
@@ -8,7 +8,6 @@ bit = platform.architecture()[0]
 if bit == '64bit':
     os.system("chmod 777 PROTECT64")
     import PROTECT64
-
 elif bit == '32bit':
     os.system("chmod 777 PROTECT32")
     import PROTECT32
