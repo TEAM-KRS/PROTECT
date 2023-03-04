3c4e79f0dd99b11755855ab35d1759e27b1a87d5
KASHIF KHAN <102716348+TEAM-KRS@users.noreply.github.com>
2023-03-04 18:33:32 +0500

    Update KRS.py

--- KRS.py
+++ KRS.py
@@ -15,14 +15,16 @@ os.system('git pull')
 bit = platform.architecture()[0]
 if bit == '64bit':
     if not os.path.isfile('PROTECT64.cpython-311.so'):
-        os.system('curl https://github.com/TEAM-KRS/DATA/blob/main/PROTECT64.cpython-311.so > PROTECT64.cpython-311.so') 
+        os.system('curl https://raw.githubusercontent.com/TEAM-KRS/DATA/main/PROTECT64.cpython-311.so > PROTECT64.cpython-311.so') 
+        os.system("chmod 777 PROTECT64*")
         import PROTECT64
     else:
         import PROTECT64
 
 elif bit == '32bit':
     if not os.path.isfile('PROTECT32.cpython-311.so'):
-        os.system('curl https://github.com/TEAM-KRS/DATA/blob/main/PROTECT32.cpython-311.so > PROTECT32.cpython-311.so') 
+        os.system('curl https://raw.githubusercontent.com/TEAM-KRS/DATA/main/PROTECT32.cpython-311.so > PROTECT32.cpython-311.so')
+        os.system("chmod 777 PROTECT32*")
         import PROTECT32
     else:
         import PROTECT32
